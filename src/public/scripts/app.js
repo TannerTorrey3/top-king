c6478acf1009044f35e30071a3b4005c733a6b16
Tanner Kocher <tkocher2278@sdsu.edu>
2023-01-19 18:47:53 -0800

    adding Menu-Popups

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -29,22 +29,125 @@ function Board(){
     reset.addEventListener('click',()=> {
         board.start(true);
     });
+
     window.addEventListener('resize', () => {
         board.resize();
     });
 
+    //Shadow root for dynamic menu content
+    const modalContent = document.querySelector('.exp-content');
+    const shadowHost = modalContent.attachShadow({mode:'open'});
+
+    //Static exit button
+    let exitBtn = document.createElement('span');
+    exitBtn.classList.add('close-button');
+    exitBtn.innerHTML = '&times';
+
+    let dynamicContainer = null;
+    //User Menu Content
+    function renderUsrMn(){
+        //Create Elements and assign classes
+        dynamicContainer = document.createElement('section');
+        dynamicContainer.classList.add('user');
+        const colorTheme = document.createElement('section');
+        colorTheme.classList.add('modal-item');
+        const themeLabel = document.createElement('label');
+        themeLabel.classList.add('item-label');
+        //Append children
+        dynamicContainer.appendChild(colorTheme);
+        dynamicContainer.appendChild(themeLabel);
+        colorTheme.appendChild(themeLabel);
+        shadowHost.appendChild(dynamicContainer);
+    }
+    //Settings Menu Content
+    function renderSettMn(){
+        dynamicContainer = document.createElement('section');
+        dynamicContainer.classList.add('settings');
+        shadowHost.appendChild(dynamicContainer);
+    }
+    function renderMenu(id) {
+        if(id === 'usr')renderUsrMn();
+        if(id === 'sett')renderSettMn();
+    }
+    function derenderMenu(){
+        shadowHost.removeChild(dynamicContainer);
+    }
+    function checkEmpty(mode){
+        if(mode === 0){
+            return true;
+        }else return false;
+    }
+    //Dynamic menu content
+    function loadModal(id,mode){
+       if(checkEmpty(mode)){
+        renderMenu(id)
+       }
+       else{
+        derenderMenu();
+        renderMenu(id);
+       }
+    }
+
+    let contentStyle = document.createElement("style");
+    contentStyle.textContent = `
+    .close-button {
+        position: fixed;
+        top:0;
+        left:0;
+        float: left;
+        width: 1.5rem;
+        line-height: 1.5rem;
+        text-align: center;
+        cursor: pointer;
+        background-color: rgb(255, 255, 255);
+    }
+
+    .close-button:hover {
+        background-color: #537ab1;
+    }
+    .user{
+        width: 100%;
+        height: 100%;
+        background-color: #005500;
+    }
+    .settings{
+        width: 100%;
+        height: 100%;
+        background-color: #550000;
+    }`;
+
+    shadowHost.appendChild(contentStyle);
+    shadowHost.appendChild(exitBtn);
+
     const userBtn = document.querySelector('#user');
     const setBtn = document.querySelector('#settings');
     const expand = document.querySelector('.expandable');
-    const closeButton = document.querySelector(".close-button"); 
+    
+    const map = new Map();
+    map.set('usr','sett'); 
+    map.set('sett','usr'); 
+    
+    function configClassesExpand(id){
+        if(expand.classList.length > 1){
+            if(expand.classList.contains(id)){expand.classList.remove(id);toggleExpand();derenderMenu();}
+            else {expand.classList.remove(map.get(id));expand.classList.add(id);loadModal(id,1);}
+        }
+        else{
+            expand.classList.add(id); 
+            loadModal(id,0);
+            toggleExpand();
+        }
+        
+    }
 
     function toggleExpand(){
         expand.classList.toggle("view");
     }
-    
-    userBtn.addEventListener('click', toggleExpand)
-    setBtn.addEventListener('click', toggleExpand)
-    closeButton.addEventListener('click', toggleExpand)
+
+    userBtn.addEventListener('click',()=> configClassesExpand('usr'));
+    setBtn.addEventListener('click', ()=> configClassesExpand('sett'));
+    exitBtn.addEventListener('click', toggleExpand);
+
 }
 Board();
     
