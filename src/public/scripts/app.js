90e84dd47fce9f773572e6ac16626a2fa4e00aed
Tanner Kocher <tkocher2278@sdsu.edu>
2023-01-23 17:13:23 -0800

    adding Chess Logic

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -12,21 +12,82 @@ function Board(){
     let board = null;
 
     const game = new Chess();
-    console.log(`${game.fen()}`);
+
+    function onDragStart (source, piece, position, orientation) {
+        // do not pick up pieces if the game is over
+        if (game.isGameOver()) return false
+      
+        // only pick up pieces for the side to move
+        if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+            (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+          return false
+        }
+    }
+    
+    function updateStatus () {
+        var status = ''
+      
+        var moveColor = 'White'
+        if (game.turn() === 'b') {
+          moveColor = 'Black'
+        }
+      
+        // checkmate?
+        if (game.isCheckmate()) {
+          status = 'Game over, ' + moveColor + ' is in checkmate.'
+        }
+      
+        // draw?
+        else if (game.isDraw()) {
+          status = 'Game over, drawn position'
+        }
+      
+        // game still on
+        else {
+          status = moveColor + ' to move'
+      
+          // check?
+          if (game.isCheck()) {
+            status += ', ' + moveColor + ' is in check'
+          }
+        }
+      
+        document.querySelector('#status').innerHTML = `${status}`;
+        document.querySelector('#fen').innerHTML = `${game.fen()}`;
+    }
+    function onDrop (source, target) {
+        // see if the move is legal
+        try {
+            let move =game.move({
+                from: source,
+                to: target // NOTE: always promote to a queen for example simplicity
+            })
+        }
+        catch(isIllegal){
+            board.position(game.fen())
+        }
+        // illegal move
+        
+        updateStatus()
+    }
+
+    function onSnapEnd () {
+        board.position(game.fen())
+    }
 
     const config = {
         draggable: true,
         position: 'start',
         showNotation: false,
         showErrors: true,
-        onDrop: onDrop
+        dropOffBoard: 'snapback',
+        onDragStart: onDragStart,
+        onDrop: onDrop,
+        onSnapEnd: onSnapEnd
     }
 
-    board = Chessboard("myBoard", config)
-
-    function onDrop (source, target, piece, newPos, oldPos, orientation) {
-        console.log(`${source}-${target}`)
-    }
+    board = Chessboard("myBoard", config);
+    updateStatus();
 
     const reset = document.querySelector("#res");
     reset.addEventListener('click',()=> {
